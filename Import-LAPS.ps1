6fb2da281bb029649b9e7a4102175769f687aa6a
James Smith <psych0ticus.aus@gmail.com>
2017-01-31 23:01:34 +1100

    reverted to PS import-GPO

--- Import-LAPS.ps1
+++ Import-LAPS.ps1
@@ -1,38 +1,38 @@
 #requires -version 3 -RunAsAdministrator
 
-param ( [string]$ComputerOU, 
-        [string]$OrgUnitRead, 
-        [string]$OrgUnitReset, 
-        [string]$SecGroupRead, 
-        [string]$SecGroupReset,
-        [string]$GPOName
-      )
+param ([string]$ComputerOU,
+	[string]$OrgUnitRead,
+	[string]$OrgUnitReset,
+	[string]$SecGroupRead,
+	[string]$SecGroupReset,
+	[string]$GPOName
+)
 
 # Find Netbios name of domain.
 $NetBIOSName = (Get-ADDomain).NetBIOSName
 $FQDN = (Get-ADDomain).DNSRoot
 
 
-IF(!(Test-Path $env:homedrive\Shadow\LAPS))
-    {
-        New-Item "$env:homedrive\Shadow\LAPS" -ItemType Directory -Force
-    }
+IF (!(Test-Path $env:homedrive\Shadow\LAPS))
+{
+	New-Item "$env:homedrive\Shadow\LAPS" -ItemType Directory -Force
+}
 
 # Start a transcipt of the script.
 Start-Transcript "C:\Shadow\LAPS\Script.log"
 
 # Create share on the DC for the software. Add domain computers read access to share.
-New-SmbShare -Name "LAPS$" -Path "$env:homedrive\Shadow\LAPS\" -ReadAccess "$NetBIOSName\Domain computers"
-      
-IF(!(Test-Path $env:homedrive\Shadow\LAPS))
-    {
-        Copy-Item -Path .\ -Destination "$env:homedrive\Shadow\LAPS" -Recurse
-    }
+New-SmbShare -Name "LAPS$" -Path "$env:homedrive\Shadow\LAPS\" -ReadAccess "$NetBIOSName\Domain computers" -FullAccess "$NetBIOSName\Domain Admins"
+
+IF (!(Test-Path $env:homedrive\Shadow\LAPS))
+{
+	Copy-Item -Path .\ -Destination "$env:homedrive\Shadow\LAPS" -Recurse
+}
 
 IF (!($GPOName))
-    {
-        $GPOName = "Deploy-LAPS"
-    }
+{
+	$GPOName = "Deploy-LAPS"
+}
 
 
 # Download and install the MS LAPS software.
@@ -57,81 +57,77 @@ Update-AdmPwdADSchema
 $OUQuery = [adsisearcher]'(&(objectclass=domain))'
 $OUQuery.SearchScope = 'base'
 $OUQuery.FindOne().properties.wellknownobjects | ForEach-Object {
-    if ($_ -match '^B:32:AA312825768811D1ADED00C04FD8D5CD:(.*)$')
-            {
-                $Matches[1]
-            }
-        }
+	if ($_ -match '^B:32:AA312825768811D1ADED00C04FD8D5CD:(.*)$')
+	{
+		$Matches[1]
+	}
+}
 
 
 # Check if a computer OU was provided by the parameter and act accordingly.
 if (!$ComputerOU)
-    {
-        Set-AdmPwdComputerSelfPermission -OrgUnit $Matches[1]
-    }
-    else
-        {
-            Set-AdmPwdComputerSelfPermission -OrgUnit $ComputerOU
-        }
+{
+	Set-AdmPwdComputerSelfPermission -OrgUnit $Matches[1]
+}
+else
+{
+	Set-AdmPwdComputerSelfPermission -OrgUnit $ComputerOU
+}
 
 # Configure who can read the attribute. By default only domain/enterprise admins can.
 if ($OrgUnitRead)
-    {
-        Set-AdmPwdReadPasswordPermission -OrgUnit $OrgUnitRead
-    }
+{
+	Set-AdmPwdReadPasswordPermission -OrgUnit $OrgUnitRead
+}
 if ($SecGroupRead)
-    {
-        Set-AdmPwdReadPasswordPermission -AllowedPrincipals $SecGroupRead
-    }
+{
+	Set-AdmPwdReadPasswordPermission -AllowedPrincipals $SecGroupRead
+}
 
 # Configure who can force a password change. By default only domain/enterprise admins can.
 if ($OrgUnitReset)
-    {
-        Set-AdmPwdResetPasswordPermission -OrgUnit $OrgUnitReset
-    }
+{
+	Set-AdmPwdResetPasswordPermission -OrgUnit $OrgUnitReset
+}
 if ($SecGroupReset)
-    {
-        Set-AdmPwdResetPasswordPermission -AllowedPrincipals $SecGroupReset
-    }
+{
+	Set-AdmPwdResetPasswordPermission -AllowedPrincipals $SecGroupReset
+}
 
 
 # Importing the GPO
 # Reference https://gallery.technet.microsoft.com/Migrate-Group-Policy-2b5067d8#content
 Set-Location "$env:homedrive\Shadow\LAPS"
-Import-Module GroupPolicy            
-Import-Module ActiveDirectory            
-# change vars in csv to suit environment
-$MigrationMap = Import-Csv .\MigrationMap.csv 
+Import-Module GroupPolicy
+Import-Module ActiveDirectory
 
-$Output = ForEach ($r in $MigrationMap) 
-    {
-        if ($r.Destination -like "DestDomainFQDN")
-            {
-                $r.Destination = "$FQDN"
-                $r
-            }
-            elseif ($r.Destination -like "DestDomainNetBIOS")
-                {
-                    $r.Destination = "$NetBIOSName"
-                    $r
-                }
-            elseif ($r.Destination -like "DestDomainFQDNUNC")
-                {
-                    $r.Destination = "\\$FQDN\"
-                    $r
-                }
-            elseif ($r.Destination -like "DestDomainNetBIOSUNC")
-                {
-                    $r.Destination = "\\$NetBIOSName\"
-                    $r
-                }
-    }
-    
-$Output | Export-CSV .\MigrationMap.csv -NoTypeInformation
+# change variables in the GPO to suit environment using a GPO migration table.
+$Path = "$env:homedrive\Shadow\LAPS\MigrationTable.migtable"
+$XML = [xml] (Get-Content $Path)
+$Destination = $XML.MigrationTable.Mapping.ChildNodes
 
-            
-        
- 
+foreach ($node in $Destination)
+{
+	if ($node.'#text' -eq "ENTERPRISEADMINS")
+	{
+		$Node.innertext = "Enterprise Admins@$FQDN"
+	}
+	if ($node.'#text' -eq "DOMAINADMINS")
+	{
+		$Node.innertext = "Domain Admins@$FQDN"
+	}
+	if ($node.'#text' -eq "\\SERVERNAME\LAPS$")
+	{
+		$Node.innertext = "\\$env:computername\LAPS$\LAPSx64.msi"
+	}
+}
+$XML.Save("$env:homedrive\Shadow\LAPS\LAPS.migtable")
+
+
+
+
+
 #Import the actual GPO            
-Import-GPO -CreateIfNeeded -path $env:homedrive\Shadow\LAPS\ -BackupId "{4178CB42-3A58-445C-A46E-9CD8338C9FA5}" -TargetName $GPOName -MigrationTable $env:homedrive\Shadow\LAPS\MigrationMap.csv
-    
+#http://serverfault.com/questions/491505/powershell-copy-gpo-failing-with-hresult-0x8007000d
+#cscript importgpo.wsf "$env:homedrive\Shadow\LAPS\" "{4178CB42-3A58-445C-A46E-9CD8338C9FA5}" /CreateifNeeded /migrationtable:C:\Shadow\LAPS\LAPS.migtable
+Import-GPO -CreateIfNeeded -path $env:homedrive\Shadow\LAPS\ -BackupId "{4178CB42-3A58-445C-A46E-9CD8338C9FA5}" -TargetName $GPOName -MigrationTable "$env:homedrive\Shadow\LAPS\LAPS.migtable"
